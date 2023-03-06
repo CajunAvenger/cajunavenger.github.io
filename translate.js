f060597bb725af9933860f8003008d203a304ef4
CajunAvenger <kajunkittyavenger@gmail.com>
2023-03-06 13:43:39 -0600

    translation prep

---
+++ translate.js
@@ -0,0 +1,310 @@
+var "languages": [
+	"English",
+	"Español",
+	"Português",
+	"Deutsch",
+	"日本",
+	"简中",
+	"繁中",
+	"Français",
+	"Italiano",
+	"한국어"
+];
+var regional_suffix = ["Español", "Português", "Français", "Italiano"];
+var no_spaces = ["日本", "简中", "繁中"];
+var translatable = {
+	"Randomize!": {
+		"English": ["Randomize!"],
+		"Español": ["Aleatorizar!"],
+		"Português": ["Aleatória!"],
+		"Deutsch": ["Randomisieren!"],
+		"日本": ["ランダマイズ！"],
+		"简中": ["随机化！"],
+		"繁中": ["隨機化！"],
+		"Français": ["Aléatoire!"],
+		"Italiano": ["Casuale!"],
+		"한국어": ["무작위의!"]
+	},
+	"Shiny Charm1": {
+		"English": "Click the Shiny Charm, then click Pokemon to make them shiny.",
+		"Español": "Haz clic en Amuleto Iris, luego haz clic en Pokémon para hacerlos variocolor.",
+		"Português": "Clique no Amuleto Iris e, em seguida, clique em Pokemon para torná-los brilhantes.",
+		"Deutsch": "Klicken Sie auf den Schillerpin und dann auf Pokemon, um sie glänzend zu Schillerndes.",
+		"日本": "ひかるおまもりをクリックしてから、ポケモンをクリックして光るます。",
+		"简中": "点击闪耀护符然后点击宝可梦让它们发光。",
+		"繁中": "單擊闪耀护符然後單擊宝可梦使它們發光。",
+		"Français": "Cliquez sur le Charme Chroma, puis cliquez sur Pokemon pour les rendre chromatique.",
+		"Italiano": "Fai clic sul Cromamuleto , quindi fai clic su Pokemon per renderli cromatico.",
+		"한국어": "빛나는부적 을 클릭한 다음 포켓몬 을 클릭하여 반짝이게 만들 빛나는"
+	},
+	// unfinished
+	"Shiny Charm2": {
+		"English": "Or click here to make everything shiny.",
+		"Español": "",
+		"Português": "",
+		"Deutsch": "",
+		"日本": "",
+		"简中": "",
+		"繁中": "",
+		"Français": "",
+		"Italiano": "",
+		"한국어": ""
+	},
+	"Team": {
+		"English": "Team",
+		"Español": "Equipo",
+		"Português": "Equipa",
+		"Deutsch": "Team",
+		"日本": "手持ちポケモン",
+		"简中": "同行",
+		"繁中": "同行",
+		"Français": "Équipe",
+		"Italiano": "Squadra",
+		"한국어": "지닌 포켓몬"
+	},
+	// masc / femme
+	"Favorite": {
+		"English": ["Favorite", "Favorite"],
+		"Español": ["Favorito", "Favorita"],
+		"Português": ["Favorito", "Favorita"],
+		"Deutsch": ["Favorit", "Favoritin"],
+		"日本": ["お気に入り", "お気に入り"],
+		"简中": ["最喜欢的","最喜欢的"],
+		"繁中": ["最喜歡的", "最喜歡的"],
+		"Français": ["Préféré", "Préférée"],
+		"Italiano": ["Preferito", "Preferita"],
+		"한국어": ["가장 좋아하는", "가장 좋아하는"]
+	},
+	"Regional": {
+		"English": "Regional",
+		"Español": "regional",
+		"Português": "regional",
+		"Deutsch": "Regional",
+		"日本": "地域",
+		"简中": "地区",
+		"繁中": "地區",
+		"Français": "régionale",
+		"Italiano": "regionale",
+		"한국어": "지역"
+	},
+	"Bird": {
+		"English": "Bird",
+		"Español": "Pájaro",
+		"Português": "Pássaro",
+		"Deutsch": "Vogel",
+		"日本": "鳥",
+		"简中": "鸟",
+		"繁中": "鳥",
+		"Français": "Oiseau",
+		"Italiano": "Uccello",
+		"한국어": "새"
+	},
+	"Bug": {
+		"English": "Bug",
+		"Español": "Bicho",
+		"Português": "Erro",
+		"Deutsch": "Insekt",
+		"日本": "バグ",
+		"简中": "漏洞",
+		"繁中": "漏洞",
+		"Français": "Bogue",
+		"Italiano": "Insetto",
+		"한국어": "벌레"
+	},
+	"Mammal": {
+		"English": "Mammal",
+		"Español": "Mamífero",
+		"Português": "Mamífero",
+		"Deutsch": "Säugetier",
+		"日本": "哺乳類",
+		"简中": "哺乳动物",
+		"繁中": "哺乳動物",
+		"Français": "Mammifère",
+		"Italiano": "Mammifero",
+		"한국어": "포유류"
+	},
+	// unfinished
+	"Pseudo Legend": {
+		"English": "",
+		"Español": "",
+		"Português": "",
+		"Deutsch": "",
+		"日本": "600族",
+		"简中": "",
+		"繁中": "",
+		"Français": "",
+		"Italiano": "",
+		"한국어": ""
+	},
+	// guesstimated
+	"Pikachu Clone": {
+		"English": "Pikachu Clone",
+		"Español": "Pikachu Clon",
+		"Português": "Pikachu Clone",
+		"Deutsch": "Pikachuklon",
+		"日本": "ピカチュウのクローン",
+		"简中": "皮卡丘克隆",
+		"繁中": "皮卡丘克隆",
+		"Français": "Pikachu Cloner",
+		"Italiano": "",
+		"한국어": "피카츄 클론"
+	},
+	// unfinished
+	"Eeveelution": {
+		"English": "",
+		"Español": "",
+		"Português": "",
+		"Deutsch": "",
+		"日本": "",
+		"简中": "",
+		"繁中": "",
+		"Français": "",
+		"Italiano": "",
+		"한국어": ""
+	},
+	"Fossil": {
+		"English": "Fossil",
+		"Español": "Fósil",
+		"Português": "Fóssil",
+		"Deutsch": "Fossil",
+		"日本": "化石",
+		"简中": "化石",
+		"繁中": "化石",
+		"Français": "Fossile",
+		"Italiano": "Fossile",
+		"한국어": "화석"
+	},
+	"Regional Form": {
+		"English": "Regional Form",
+		"Español": "Forma regional",
+		"Português": "Forma regional",
+		"Deutsch": "Regionalform",
+		"日本": "リージョンフォーム",
+		"简中": "地區型態",
+		"繁中": "地區型態",
+		"Français": "Forme régionale",
+		"Italiano": "Forma regionale",
+		"한국어": "리전폼"
+	},
+	"Gigantamax": {
+		"English": "Gigantamax",
+		"Español": "Gigamax",
+		"Português": "Gigantamax",
+		"Deutsch": "Gigadynamax",
+		"日本": "キョダイマックス",
+		"简中": "超极巨化",
+		"繁中": "超極巨化",
+		"Français": "Gigamax",
+		"Italiano": "Gigamax",
+		"한국어": "거다이맥스"
+	},
+	// unfinished
+	"Box Legendary": {
+		"English": "",
+		"Español": "",
+		"Português": "",
+		"Deutsch": "",
+		"日本": "",
+		"简中": "",
+		"繁中": "",
+		"Français": "",
+		"Italiano": "",
+		"한국어": ""
+	},
+	"Mythical": {
+		"English": "Mythical",
+		"Español": "Pokémon mítico",
+		"Português": "Pokémon mítico",
+		"Deutsch": "Mysteriöse Pokémon",
+		"日本": "幻のポケモン",
+		"简中": "幻之宝可梦",
+		"繁中": "幻之寶可夢",
+		"Français": "Pokémon fabuleux",
+		"Italiano": "Pokémon misterioso",
+		"한국어": "환상의 포켓몬"
+	},
+	"Ultra Beast": {
+		"English": "Ultra Beast",
+		"Español": "Ultraente",
+		"Português": "Ultrafera",
+		"Deutsch": "Ultrabestie",
+		"日本": "ウルトラビースト",
+		"简中": "究极异兽",
+		"繁中": "究極異獸",
+		"Français": "Ultra-Chimère",
+		"Italiano": "Ultracreatura",
+		"한국어": "울트라비스트"
+	},
+	"Paradox": {
+		"English": "Paradox",
+		"Español": "Paradoja",
+		"Português": "Paradoxo",
+		"Deutsch": "Paradox",
+		"日本": "パラドックス",
+		"简中": "悖謬",
+		"繁中": "悖謬",
+		"Français": "Paradoxe",
+		"Italiano": "Paradosso",
+		"한국어": "패러독스"
+	},
+	"Type": {
+		"English": "Type",
+		"Español": "Tipo",
+		"Português": "Tipo",
+		"Deutsch": "Typ",
+		"日本": "タイプ",
+		"简中": "属性",
+		"繁中": "屬性",
+		"Français": "Type",
+		"Italiano": "Tipo",
+		"한국어": "타입"
+	},
+	"Pokeball": {
+		"English": "Poké Ball",
+		"Español": "Poké Ball",
+		"Português": "Pokébola",
+		"Deutsch": "Pokéball",
+		"日本": "モンスターボール",
+		"简中": "精灵球",
+		"繁中": "精靈球",
+		"Français": "Poké Ball",
+		"Italiano": "Poké Ball",
+		"한국어": "몬스터볼"
+	},
+	"Spinda": {
+		"English": "Spinda",
+		"Español": "Spinda",
+		"Português": "Spinda",
+		"Deutsch": "Pandir",
+		"日本": "パッチール",
+		"简中": "晃晃斑",
+		"繁中": "晃晃斑",
+		"Français": "Spinda",
+		"Italiano": "Spinda",
+		"한국어": " 	얼루기"
+	},
+	"Vivillon": {
+		"English": "Vivillon",
+		"Español": "Vivillon",
+		"Português": "Vivillon",
+		"Deutsch": "Vivillon",
+		"日本": "ビビヨン",
+		"简中": "彩粉蝶",
+		"繁中": "彩粉蝶",
+		"Français": "Prismillon",
+		"Italiano": "Vivillon",
+		"한국어": "비비용"
+	},
+	"Alcremie": {
+		"English": "Alcremie",
+		"Español": "Alcremie",
+		"Português": "Alcremie",
+		"Deutsch": "Pokusan",
+		"日本": "マホイップ",
+		"简中": "霜奶仙",
+		"繁中": "霜奶仙",
+		"Français": "Charmilly",
+		"Italiano": "Alcremie",
+		"한국어": "마휘핑"
+	}
+}
